540aa90e8a85739445de39083a0e861e5fe6dee8
citrayuda <yudacitra89@gmail.com>
2025-03-19 22:41:33 +0700

    pribadi

--- src/hallo2/Hallo2.java
+++ src/hallo2/Hallo2.java
@@ -16,6 +16,7 @@ public class Hallo2 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello word");
+        System.out.println("yuda");
     }
    
 }
